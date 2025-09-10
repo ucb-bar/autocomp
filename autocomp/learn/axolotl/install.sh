6dc17275276c9c8d49af810c16ec22265d51e066
Charles Hong <charleshong@berkeley.edu>
2025-09-09 17:31:43 -0700

    update

--- autocomp/learn/axolotl/install.sh
+++ autocomp/learn/axolotl/install.sh
@@ -1,3 +1,5 @@
 pip3 install torch==2.6.0
 pip3 install -U packaging==23.2 setuptools==75.8.0 wheel ninja
 pip3 install --no-build-isolation axolotl[flash-attn,deepspeed]
+pip install "cut-cross-entropy[transformers] @ git+https://github.com/axolotl-ai-cloud/ml-cross-entropy.git@0ee9ee8"
+conda install -c conda-forge libstdcxx-ng
