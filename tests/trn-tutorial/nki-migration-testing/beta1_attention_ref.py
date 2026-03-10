a6f05a1aa66834078e02bc46074fa515792ec8aa
Jeremy Mills <jeremymills496@gmail.com>
2026-03-10 10:03:15 +0000

    tests for attention kernel

---
+++ tests/trn-tutorial/nki-migration-testing/beta1_attention_ref.py
@@ -0,0 +1,124 @@
+import math
+
+import numpy as np
+import neuronxcc.nki as nki
+import neuronxcc.nki.isa as nisa
+import neuronxcc.nki.language as nl
+
+
+@nki.jit
+def ref(q, k, v):
+  """Beta 1 attention reference kernel (from `4_attention_test.py`)."""
+  d_head, seqlen_q = q.shape
+  seqlen_kv = seqlen_q
+
+  PMAX = nl.tile_size.pmax
+  FMAX_STATIONARY = nl.tile_size.gemm_stationary_fmax
+  FMAX_MOVING = nl.tile_size.gemm_moving_fmax
+
+  assert q.shape == k.shape == v.shape
+  assert d_head == PMAX
+  assert seqlen_q >= 512
+
+  kernel_out = nl.ndarray((seqlen_q, d_head), dtype=q.dtype, buffer=nl.shared_hbm)
+
+  # load inputs into SBUF
+  q_sbuf = nl.load(q)
+  k_sbuf = nl.load(k)
+  v_sbuf = nl.load(v)
+
+  qk = nl.ndarray((seqlen_q // PMAX, seqlen_kv // FMAX_MOVING, nl.par_dim(PMAX), FMAX_MOVING),
+                  dtype=nl.float32, buffer=nl.psum)
+  for i_tile_q in nl.affine_range(seqlen_q // FMAX_STATIONARY):
+    for i_tile_kv in nl.affine_range(seqlen_kv // FMAX_MOVING):
+      qk[i_tile_q, i_tile_kv, :, :] = nisa.nc_matmul(
+        stationary=q_sbuf[0:PMAX, nl.ds(i_tile_q * FMAX_STATIONARY, FMAX_STATIONARY)],
+        moving=k_sbuf[0:PMAX, nl.ds(i_tile_kv * FMAX_MOVING, FMAX_MOVING)])
+
+  # reduce max along seqlen_k
+  row_max = nl.ndarray((nl.par_dim(PMAX), seqlen_q // PMAX, 1), dtype=nl.float32, buffer=nl.sbuf)
+  for i_tile_q in nl.affine_range(seqlen_q // PMAX):
+    row_max_kv = nl.ndarray((nl.par_dim(PMAX), seqlen_kv // FMAX_MOVING), dtype=nl.float32, buffer=nl.sbuf)
+    for i_tile_kv in nl.affine_range(seqlen_kv // FMAX_MOVING):
+      row_max_kv[:, i_tile_kv] = nisa.tensor_reduce(op=nl.max, data=qk[i_tile_q, i_tile_kv], axis=1)
+    row_max[:, i_tile_q, :] = nisa.tensor_reduce(op=nl.max, data=row_max_kv[:, :], axis=1)
+
+  # subtract max from row
+  norm_row = nl.ndarray((seqlen_q // PMAX, PMAX, seqlen_kv),
+                        dtype=nl.float32, buffer=nl.shared_hbm)
+  for i_tile_q in nl.affine_range(seqlen_q // PMAX):
+    norm_buf = nl.ndarray(shape=(nl.par_dim(PMAX), seqlen_kv), dtype=nl.float32, buffer=nl.sbuf)
+    for i_tile_kv in nl.affine_range(seqlen_kv // FMAX_MOVING):
+      norm_buf[:, nl.ds(i_tile_kv * FMAX_MOVING, FMAX_MOVING)] = nisa.tensor_scalar(
+        data=qk[i_tile_q, i_tile_kv],
+        op0=nl.subtract,
+        operand0=row_max[:, i_tile_q, :],
+        engine=nisa.vector_engine)
+    nl.store(norm_row[i_tile_q], norm_buf[:, :])
+
+  # exponentiation
+  exp_row = nl.ndarray((seqlen_q // PMAX, PMAX, seqlen_kv), dtype=nl.float32, buffer=nl.shared_hbm)
+  for i_tile_q in nl.affine_range(seqlen_q // PMAX):
+    exp_buf = nl.ndarray(shape=(nl.par_dim(PMAX), seqlen_kv), dtype=nl.float32, buffer=nl.sbuf)
+    norm_buf = nl.load(norm_row[i_tile_q])
+    exp_buf[:, :] = nisa.activation(op=nl.exp, data=norm_buf)
+    nl.store(exp_row[i_tile_q], exp_buf[:, :])
+
+  # sum of exp results
+  sum_row = nl.ndarray((nl.par_dim(PMAX), seqlen_q // PMAX), dtype=nl.float32, buffer=nl.sbuf)
+  for i_tile_q in nl.affine_range(seqlen_q // PMAX):
+    exp_buf = nl.load(exp_row[i_tile_q])
+    sum_row[:, i_tile_q] = nisa.tensor_reduce(op=nl.add, data=exp_buf, axis=1)
+
+  inverse_sum_row = nisa.reciprocal(data=sum_row)
+
+  scores = nl.ndarray((seqlen_q // PMAX, PMAX, seqlen_kv), dtype=nl.float32, buffer=nl.shared_hbm)
+  for i_tile_q in nl.affine_range(seqlen_q // PMAX):
+    scores_buf = nl.ndarray(shape=(nl.par_dim(PMAX), seqlen_kv), dtype=nl.float32, buffer=nl.sbuf)
+    exp_buf = nl.load(exp_row[i_tile_q])
+    scores_buf[:, :] = nisa.tensor_scalar(data=exp_buf,
+                                          op0=nl.multiply,
+                                          operand0=inverse_sum_row[:, i_tile_q],
+                                          engine=nisa.vector_engine,
+                                          dtype=nl.float32)
+    nl.store(scores[i_tile_q], scores_buf[:, :])
+
+  # v has the wrong layout
+  v_t = nl.ndarray((seqlen_kv // PMAX, PMAX, d_head), dtype=nl.float32, buffer=nl.shared_hbm)
+  for i_tile_kv in nl.affine_range(seqlen_kv // PMAX):
+    v_psum_t = nisa.nc_transpose(v_sbuf[:, nl.ds(i_tile_kv * PMAX, PMAX)])
+    v_sbuf_t = nl.ndarray((nl.par_dim(PMAX), d_head), dtype=nl.float32, buffer=nl.sbuf)
+    v_sbuf_t[:, :] = nisa.tensor_copy(v_psum_t, dtype=nl.float32)
+    nl.store(v_t[i_tile_kv], v_sbuf_t[:, :])
+
+  scores_t = nl.ndarray((seqlen_kv // PMAX, seqlen_q // PMAX, PMAX, PMAX), dtype=nl.float32, buffer=nl.shared_hbm)
+  for i_tile_q in nl.affine_range(seqlen_q // PMAX):
+    for i_tile_kv in nl.affine_range(seqlen_kv // PMAX):
+      scores_buf = nl.load(scores[i_tile_q, :, nl.ds(i_tile_kv * PMAX, PMAX)])
+      scores_psum_t = nisa.nc_transpose(scores_buf)
+      scores_sbuf_t = nl.ndarray((nl.par_dim(PMAX), PMAX), dtype=nl.float32, buffer=nl.sbuf)
+      scores_sbuf_t[:, :] = nisa.tensor_copy(scores_psum_t, dtype=nl.float32)
+      nl.store(scores_t[i_tile_kv, i_tile_q, :, :], scores_sbuf_t)
+
+  # scores @ V
+  for i_tile_q in nl.affine_range(seqlen_q // PMAX):
+    attn_out_psum = nl.zeros((PMAX, PMAX), dtype=nl.float32, buffer=nl.psum)
+    attn_out = nl.ndarray((nl.par_dim(PMAX), d_head),
+                          dtype=nl.float32, buffer=nl.sbuf)
+    for i_tile_kv in nl.affine_range(seqlen_kv // PMAX):
+      scores_sbuf_t = nl.load(scores_t[i_tile_kv, i_tile_q, :, :])
+      v_sbuf_t = nl.load(v_t[i_tile_kv, :, :])
+      attn_out_psum += nisa.nc_matmul(stationary=scores_sbuf_t, moving=v_sbuf_t)
+    attn_out[:, :] = nisa.tensor_copy(attn_out_psum)
+    nl.store(dst=kernel_out[nl.ds(i_tile_q * PMAX, PMAX), :], value=attn_out[:, :])
+
+  return kernel_out
+
+
+if __name__ == "__main__":
+  q = np.load("attn_q.npy")
+  k = np.load("attn_k.npy")
+  v = np.load("attn_v.npy")
+  out = ref(q, k, v)
+  np.save("out_beta1_attention.npy", out)
+
