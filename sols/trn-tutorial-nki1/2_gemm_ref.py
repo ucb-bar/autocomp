c50a453f412eecbffeccad2646007f854b4a933f
Jeremy Mills <jeremymills496@gmail.com>
2026-03-17 03:03:07 +0000

    restore solutions from main branch

--- sols/trn-tutorial-nki1/2_gemm_ref.py
+++ sols/trn-tutorial-nki1/2_gemm_ref.py
@@ -1,7 +1,6 @@
 @nki.jit
 def test(lhsT, rhs):
-  """NKI kernel to compute a matrix multiplication operation while blocking the
-     free dimensions of the LHS and RHS to improve memory access pattern.
+  """NKI kernel to compute a matrix multiplication operation in a tiled manner
 
   Args:
       lhsT: an input tensor of shape [K,M], where both K and M are multiples for
@@ -23,61 +22,29 @@ def test(lhsT, rhs):
   TILE_K = nl.tile_size.pmax  # 128
   TILE_N = nl.tile_size.gemm_moving_fmax  # 512
 
-  # Define the indices (shape) of the tiles
-  i_lhsT = nl.mgrid[0:TILE_K, 0:TILE_M]
-  i_rhs = nl.mgrid[0:TILE_K, 0:TILE_N]
-  i_res = nl.mgrid[0:TILE_M, 0:TILE_N]
+  # Use affine_range to loop over tiles
+  for m in nl.affine_range(M // TILE_M):
+    for n in nl.affine_range(N // TILE_N):
+      # Allocate a tensor in PSUM
+      res_psum = nl.zeros((TILE_M, TILE_N), nl.float32, buffer=nl.psum)
 
-  # Configuring the blocking size for the free dimensions
-  TILES_IN_BLOCK_M = 2
-  TILES_IN_BLOCK_N = 2
-
-  BLOCK_M = TILE_M * TILES_IN_BLOCK_M  # 256
-  BLOCK_N = TILE_N * TILES_IN_BLOCK_N  # 1024
-
-  # the size has to be multiple of block size
-  assert M % BLOCK_M == 0
-  assert N % BLOCK_N == 0
-
-  # Loop over blocks over the M dimension
-  for m in nl.affine_range(M // BLOCK_M):
-    # Load TILES_IN_BLOCK_M columns tiles from lhsT
-    lhsT_tiles = nl.ndarray(
-        (TILES_IN_BLOCK_M, K // TILE_K, nl.par_dim(TILE_K), TILE_M),
-        dtype=lhsT.dtype,
-        buffer=nl.sbuf)
-    for bm in nl.affine_range(TILES_IN_BLOCK_M):
       for k in nl.affine_range(K // TILE_K):
-        lhsT_tiles[bm, k, i_lhsT.p, i_lhsT.x] = nl.load(
-            lhsT[k * TILE_K + i_lhsT.p,
-                 (m * TILES_IN_BLOCK_M + bm) * TILE_M + i_lhsT.x])
+        # Declare the tiles on SBUF
+        lhsT_tile = nl.ndarray((TILE_K, TILE_M), dtype=lhsT.dtype, buffer=nl.sbuf)
+        rhs_tile = nl.ndarray((TILE_K, TILE_N), dtype=rhs.dtype, buffer=nl.sbuf)
 
-    for n in nl.affine_range(N // BLOCK_N):
-      # Load TILES_IN_BLOCK_N columns from rhs
-      rhs_tiles = nl.ndarray(
-          (TILES_IN_BLOCK_N, K // TILE_K, nl.par_dim(TILE_K), TILE_N),
-          dtype=rhs.dtype,
-          buffer=nl.sbuf)
-      for bn in nl.affine_range(TILES_IN_BLOCK_N):
-        for k in nl.affine_range(K // TILE_K):
-          rhs_tiles[bn, k, i_rhs.p, i_rhs.x] = nl.load(
-              rhs[k * TILE_K + i_rhs.p,
-                  (n * TILES_IN_BLOCK_N + bn) * TILE_N + i_rhs.x])
+        # Load tiles from lhsT and rhs
+        lhsT_tile[...] = nl.load(lhsT[k * TILE_K:(k + 1) * TILE_K,
+                                      m * TILE_M:(m + 1) * TILE_M])
+        rhs_tile[...] = nl.load(rhs[k * TILE_K:(k + 1) * TILE_K,
+                                    n * TILE_N:(n + 1) * TILE_N])
 
-      for bm in nl.affine_range(TILES_IN_BLOCK_M):
-        for bn in nl.affine_range(TILES_IN_BLOCK_N):
-          # Allocate a tensor in PSUM
-          res_psum = nl.zeros((TILE_M, TILE_N), nl.float32, buffer=nl.psum)
-          for k in nl.affine_range(K // TILE_K):
-            # Accumulate partial-sums into PSUM
-            res_psum += nl.matmul(lhsT_tiles[bm, k, i_lhsT.p, i_lhsT.x],
-                                  rhs_tiles[bn, k, i_rhs.p, i_rhs.x],
-                                  transpose_x=True)
+        # Accumulate partial-sums into PSUM
+        res_psum += nl.matmul(lhsT_tile[...], rhs_tile[...], transpose_x=True)
 
-          # Copy the result from PSUM back to SBUF, and cast to expected output data-type
-          res_sb = nl.copy(res_psum, dtype=result.dtype)
-          nl.store(result[(m * TILES_IN_BLOCK_M + bm) * TILE_M + i_res.p,
-                          (n * TILES_IN_BLOCK_N + bn) * TILE_N + i_res.x],
-                   value=res_sb)
+      # Copy the result from PSUM back to SBUF, and cast to expected output data-type
+      res_sb = nl.copy(res_psum, dtype=result.dtype)
+      nl.store(result[m * TILE_M:(m + 1) * TILE_M, n * TILE_N:(n + 1) * TILE_N],
+               value=res_sb)
 
   return result
